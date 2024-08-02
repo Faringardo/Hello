b9debc66a8978f6c1b9f12c8d60cdfc4841c9006
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-02 21:12:29 +0300

    maxLongSqr

---
+++ src/maxLongSqr.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+
+public class maxLongSqr {
+
+    public static void main(String[] args) {
+
+        BigInteger maxLong = BigInteger.valueOf(Long.MAX_VALUE);
+        BigInteger root = maxLong.sqrt();
+
+        System.out.println(root);
+    }
+}
