1211fdcd1b7e1ef2cb290a578ec05c80418d8172
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-02 20:57:28 +0300

    calcCircleRadius

---
+++ src/calcCircleRadius.java
@@ -0,0 +1,10 @@
+public class calcCircleRadius {
+
+    public static void main(String[] args) {
+
+        double area = 15;
+
+        System.out.format("%.3f", Math.sqrt(area/Math.PI));
+    }
+}
+//1.2.14
