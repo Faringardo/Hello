f9660b5d73ff2cb3e00b9870714c193794993d42
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-02 19:46:39 +0300

    Monitors

---
+++ src/drawisMonitorsCounter.java
@@ -0,0 +1,10 @@
+public class drawisMonitorsCounter {
+
+    public static void main(String[] args) {
+
+        int monitors = 456;
+        int programmers = 123;
+
+        System.out.println(monitors % programmers);
+    }
+}
