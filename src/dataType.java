cac1669fb11fca3e4662a89bd7cb7b51cda738e8
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-02 19:01:34 +0300

    DataType

---
+++ src/dataType.java
@@ -0,0 +1,14 @@
+public class dataType {
+
+    public static void main(String[] args) {
+
+        boolean varboolean = false;
+        byte varbyte = 127;
+        short varshort = 32767;
+        int varint = 2147483647;
+        long varlong = 9_223_372_036_854_775_807L;
+        double vardouble = 5.1;
+        float varfloat = 7.4f;
+        char varchar = ' ';
+    }
+}
