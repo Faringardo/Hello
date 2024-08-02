3c61ebc497df72c35f3f1618c630597cb5ed9b7f
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-02 19:59:42 +0300

    priceCalculation

---
+++ src/priceCalculation.java
@@ -0,0 +1,10 @@
+public class priceCalculation {
+
+    public static void main(String[] args) {
+
+        double price = 184.5;
+        int count = 6;
+
+        System.out.println(price * count);
+    }
+}
