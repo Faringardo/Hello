5cd9c086702e254f13253193a0ca81a3c17e3f72
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-07 00:46:39 +0300

    1.4.7

---
+++ src/factorial.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+
+public class factorial {
+
+    public static void main(String[] args) {
+        int value = 5;
+        System.out.println(factorial(value));
+    }
+
+    public static BigInteger factorial(int value) {
+
+        if (value == 0 || value == 1) {
+            return BigInteger.ONE;
+        }else {
+            return BigInteger.valueOf(value).multiply(factorial(value-1));
+        }
+    }
+}
+//1.4.7
