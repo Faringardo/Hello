03f24539295ecce60dbf56d8b3d9d5da99513583
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-07 13:35:49 +0300

    1.4.11

---
+++ src/Weekend2.java
@@ -0,0 +1,16 @@
+public class Weekend2 {
+
+    public static void main(String[] args) {
+        String weekday = "Sunday";
+
+        System.out.println(isWeekend(weekday));
+    }
+
+    public static boolean isWeekend(String weekday){
+
+        if (weekday.equals("Sunday") || weekday.equals("Saturday")){
+            return true;
+        }else return false;
+    }
+}
+//1.4.11
