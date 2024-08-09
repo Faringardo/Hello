3b9cbb0c95e783ce099196606a4e8598743d629f
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-09 19:36:06 +0300

    1.5.9

---
+++ src/PrintArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class PrintArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,3,4,5,5,6,7,7};
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
+//1.5.9
