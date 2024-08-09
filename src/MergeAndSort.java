a973e60b4c1df7ca8ef7935becff635db2750976
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-09 21:21:52 +0300

    1.5.11

--- src/MergeAndSort.java
+++ src/MergeAndSort.java
@@ -5,22 +5,15 @@ public class MergeAndSort {
     public static void main(String[] args) {
 
         int[] firstArray = {1, 3, 5, 7};
-        int[] secondArray = {2, 4, 6, 8};
+        int[] secondArray = {2, 4, 6, 6};
 
         int[] result = new int[firstArray.length + secondArray.length];
 
         System.arraycopy(firstArray, 0, result, 0, firstArray.length);
         System.arraycopy(secondArray, 0, result, firstArray.length, firstArray.length);
 
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result.length; j++) {
-                if (result[i] < result[j]) {
-                    int temp = result[i];
-                    result[i] = result[j];
-                    result[j] = temp;
-                }
-            }
-        }
+        Arrays.sort(result);
+
         System.out.println(Arrays.toString(result));
     }
 }
