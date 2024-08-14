2c94f5d9ca1d70c56679e1ad2aa4b50b7aa85ac2
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-14 14:53:31 +0300

    1.5.5

--- src/Array3.java
+++ src/Array3.java
@@ -1,19 +1,26 @@
+import java.util.Arrays;
+
 public class Array3 {
 
     public static void main(String[] args) {
 
         int [] numbers = {1,2,3,4,5,6};
 
-             if (numbers.length % 2 == 0){
-                 int[] temp = new int[2];
-                 temp[0] = numbers[numbers.length/2-1];
-                 temp[1] = numbers[numbers.length/2];
-                 System.out.println("["+temp[0]+","+temp[1]+"]");
-             }else{
-                 int[] temp2 = new int[1];
-                 temp2[0] = numbers[numbers.length/2];
-                 System.out.println("["+temp2[0]+"]");
-             }
+        System.out.println(Arrays.toString(getArrayMiddle(numbers)));
+    }
+
+    public static int[] getArrayMiddle(int[] numbers){
+        int[] temp = new int[2];
+        int[] temp2 = new int[1];
+
+        if (numbers.length % 2 == 0){
+            temp[0] = numbers[numbers.length/2-1];
+            temp[1] = numbers[numbers.length/2];
+            return temp;
+        }else{
+            temp2[0] = numbers[numbers.length/2];
+            return temp2;
+        }
     }
 }
 //1.5.5
