e7bbc13b106f41670b1faa486258718d74c3a6cc
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-14 15:01:49 +0300

    1.5.6

--- src/Rev.java
+++ src/Rev.java
@@ -1,17 +1,20 @@
+import java.util.Arrays;
+
 public class Rev {
 
     public static void main(String[] args) {
 
         int[] numbers = {1,2,3,4,5,6,7,8,9};
 
-        System.out.print("[");
+        System.out.println(Arrays.toString(inverseArray(numbers)));
+    }
 
-        System.out.print(numbers.length);
-        for (int i = 1; i < numbers.length; i++) {
-
-            System.out.print(", " + numbers[numbers.length - i - 1]);
+    public static int[] inverseArray(int[] numbers){
+        int[] rev = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            rev[numbers.length - i - 1] = numbers[i];
         }
-        System.out.println("]");
+        return rev;
     }
 }
 //1.5.6
