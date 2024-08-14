6f3e528b98363670ac8d13df45e71cd98cb61ca8
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-14 15:04:43 +0300

    1.5.7

--- src/MergeArray.java
+++ src/MergeArray.java
@@ -6,8 +6,12 @@ public class MergeArray {
     public static void main(String[] args) {
 
         int[] firstArray = {1, 3, 5, 7};
-        int[] secondArray = {2, 4, 6, 8};
+        int[] secondArray = {1, 4, 6, 8};
 
+        System.out.println(Arrays.toString(mergeAndSort(firstArray, secondArray)));
+    }
+
+    public static int[] mergeAndSort(int[] firstArray, int[] secondArray){
         int[] result = new int[firstArray.length + secondArray.length];
 
         for (int i = 0; i < firstArray.length; i++) {
@@ -26,7 +30,7 @@ public class MergeArray {
                 }
             }
         }
-        System.out.println(Arrays.toString(result));
+        return result;
     }
 }
 //1.5.7
