159788d8b158a6aa199c198cfbe81e99d14a33a6
Faringardo <58985100+Faringardo@users.noreply.github.com>
2024-08-31 15:53:16 +0300

    1.5.10

--- src/MidArray.java
+++ src/MidArray.java
@@ -6,7 +6,7 @@ public class MidArray {
 
     public static void main(String[] args) {
 
-        int [] numbers = {1,2,3,4,5,8,9};
+        int [] numbers = {1,2,3,4,5,8,9,15};
 
         System.out.println(Arrays.toString(getArrayMiddle(numbers)));
     }
@@ -14,7 +14,7 @@ public class MidArray {
     public static int[] getArrayMiddle(int[] numbers){
 
         int [] temp = new int[2 - numbers.length % 2];
-        int result = 0;
+
         if (numbers == null || numbers.length == 0) return numbers;
         if (numbers.length % 2 == 0){
             System.arraycopy(numbers,numbers.length / 2 - 1,temp,0,temp.length);
